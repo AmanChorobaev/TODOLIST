bb1cd83333ae7e3a57a0003c195eff56782a6010
Aman Chorobaev <vpupkin@company.com>
2021-01-23 13:43:03 +0600

    added todo deletions and added books form and styles

--- main/models.py
+++ main/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 
-# Create your models here.
 
-class ToDo (models.Model):
-  text = models.CharField(max_length=100)
-  created_at=models.DateField(auto_now_add=True)
-  is_closed = models.BooleanField(default=False) 
-  is_favorite = models.BooleanField(default=False)
+class ToDo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False) 
   
-class Books (models.Model):
-  name=models.CharField (max_length=100, db_index=True)
-  title=models.CharField (max_length=180)
-  subtitle=models.CharField (max_length=180)
-  description=models.CharField (max_length=180)
-  price=models.CharField (max_length=150)
-  genre=models.CharField (max_length=150)
-  author=models.CharField (max_length=150)
-  # year, models.DateField(auto_created=True)
-  date=models.DateField (auto_created=True) 
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    price = models.CharField(max_length=4)
+    genre = models.CharField(max_length=50)
+    author = models.CharField(max_length=150)
+    year = models.CharField(max_length=4)
+    date = models.DateField(auto_now_add=True) 
 
   
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
-from .models import ToDo, Books
+from django.shortcuts import render, HttpResponse, redirect
+from .models import ToDo
 
 
 def homepage(request):
@@ -7,14 +7,44 @@ def homepage(request):
 
 
 def test(request):
-    todo_list = ToDo.objects.all() 
+    todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
 
 
 def second(request):
     return HttpResponse("test 2 page")
+    
 
-def test2(request):
-    books_store = Books.objects.all() 
-    return render(request, "books.html", {"book_store": books_store})
+def books(request):
+    books_list = Books.objects.all()
+    return render(request, "books.html", {"books_list": books_list})
+
+
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
+
+
+def delete_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect(test)
+
+
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
+
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
 
